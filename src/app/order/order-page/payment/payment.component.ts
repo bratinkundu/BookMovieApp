1b556dc5926ad8529bc4b34c8d01b0c6f663e25e
bratinkundu <bkundu53@gmail.com>
2020-05-15 15:35:52 +0530

    Added the payment page

--- src/app/order/order-page/payment/payment.component.ts
+++ src/app/order/order-page/payment/payment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { PaymentService } from 'src/app/shared/Services/payment.service';
+import { CartService } from 'src/app/shared/Services/cart.service';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  otpform:FormGroup;
+  show = false;
+  errorflag = false;
+  orderid;
+  cartItems;
+  constructor(public formbuilder:FormBuilder, private router: Router, private paymentservice:PaymentService, private cartservice:CartService) { }
 
   ngOnInit(): void {
+    this.otpform = this.formbuilder.group({
+      otp : ['',[Validators.required,Validators.maxLength(6),Validators.minLength(6)]]
+    })
+  }
+
+  confirm(){
+    if(this.otpform.controls.otp.value == '778899'){
+      this.orderid = Math.floor(Math.random() * 1000000000)
+      this.show = true
+
+      this.cartservice.getItems().subscribe(
+        data =>{
+          this.cartItems = data
+          this.cartItems.forEach(element => {
+            this.cartservice.removeFromCart(element.id).subscribe()
+          });  
+          });
+
+          this.paymentservice.remove();
+       setTimeout(()=>{
+         this.router.navigate([''],) 
+       },5000) 
+    }
+    else{
+      this.errorflag = true
+    }
+  }
+
+  cancel(){
+    this.paymentservice.remove();
+    this.router.navigate(['']);
   }
 
 }
--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 import { OrderRoutingModule } from './order-routing.module';
 import { OrderPageComponent } from './order-page/order-page.component';
 import { SummaryComponent } from './order-page/summary/summary.component';
@@ -11,7 +12,9 @@ import { PaymentComponent } from './order-page/payment/payment.component';
   declarations: [OrderPageComponent, SummaryComponent, PaymentComponent],
   imports: [
     CommonModule,
-    OrderRoutingModule
+    OrderRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
   ]
 })
 export class OrderModule { }
